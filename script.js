95d94a8da383bd947c46786aeb8457af81a3e6ee
Callum White <callumwhite210@gmail.com>
2020-09-19 15:50:55 +0930

    colors

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Working");
+var today = moment().hour();
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+console.log(today);
+
+var jobs = {
+    "9":[],
+    "10":[],
+    "11":[],
+    "12":[],
+    "13":[],
+    "14":[],
+    "15":[],
+    "16":[],
+    "17":[]
+};
+//save tasks in local storage
+var saveTasks = $(".saveBtn").click (function() {
+    localStorage.setItem("tasks",JSON.stringify(jobs));
+    console.log(jobs);
+});
+//Load tasks in local storage 
+
+//add timer that changes times css
+function pastPresentFuture() {
+    $(".time").each(function(){
+        var colors = $(this).attr("id");
+        if (colors == today){
+            $(this).addClass("present");
+        }
+        else if (colors > today){
+            $(this).addClass("future");
+        }
+        else {$(this).addClass("past");
+        }
+    });
+};
+pastPresentFuture()
+//lock user out of text area when time has passed 
