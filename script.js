16334cae1432b0329205514b0ecf425af56c54db
Callum White <callumwhite210@gmail.com>
2020-09-19 16:30:40 +0930

    finish

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 console.log("Working");
 var today = moment().hour();
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+//date at top of page
 console.log(today);
-
+//Jobs var- Insert text from text info into respective hour
 var jobs = {
     "9":[],
-    "10":[],
-    "11":[],
-    "12":[],
-    "13":[],
-    "14":[],
-    "15":[],
-    "16":[],
-    "17":[]
+    "10":[""],
+    "11":[""],
+    "12":[""],
+    "13":[""],
+    "14":[""],
+    "15":[""],
+    "16":[""],
+    "17":[""]
 };
 //save tasks in local storage
-var saveTasks = $(".saveBtn").click (function() {
-    localStorage.setItem("tasks",JSON.stringify(jobs));
+var saveJobs = $(".saveBtn").click (function() {
+    localStorage.setItem("jobs",JSON.stringify(jobs));
     console.log(jobs);
 });
 //Load tasks in local storage 
 
-//add timer that changes times css
+//add timer that changes times css using moment.js
 function pastPresentFuture() {
     $(".time").each(function(){
         var colors = $(this).attr("id");
@@ -36,4 +37,5 @@ function pastPresentFuture() {
     });
 };
 pastPresentFuture()
+
 //lock user out of text area when time has passed 
